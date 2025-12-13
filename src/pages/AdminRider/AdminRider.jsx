94841ff6e46ab9033aafbef570316f9d1492e8e4
Vanexaa <reuterasvanessa@gmail.com>
2025-12-13 22:47:47 +0800

    updated vite

---
+++ src/pages/AdminRider/AdminRider.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AdminRider() {
+  return (
+    <div>
+      <h1>Admin Rider Page</h1>
+      <p>Welcome to the admin rider page!</p>
+    </div>
+  );
+}
+
+export default AdminRider;
