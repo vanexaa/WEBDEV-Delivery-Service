1c9ea0041b6489fb980ce65af3fdc15a2b5e6614
jamesmiguellll <jamesgalgana@gmail.com>
2025-12-27 20:54:37 +0800

    fixed x and back button
    fixed x and back button color and position

--- src/pages/AdminRider/AdminRider.jsx
+++ src/pages/AdminRider/AdminRider.jsx
@@ -227,8 +227,10 @@ const handleImageUpload = (e) => {
       {currentView === "riders" && (
         <div className="rider-container">
           <div className="back-btn" onClick={() => setCurrentView("dashboard")}>
-            &laquo;
-          </div>
+  <svg width="32" height="32" viewBox="0 0 24 24" fill="currentColor">
+    <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z" />
+  </svg>
+</div>
 <div className="tabs">
       {/* This is the active tab, clicking it keeps us here */}
       <span 
@@ -295,8 +297,10 @@ const handleImageUpload = (e) => {
       {currentView === "deliveries" && (
         <div className="rider-container">
           <div className="back-btn" onClick={() => setCurrentView("dashboard")}>
-            &laquo;
-          </div>
+  <svg width="32" height="32" viewBox="0 0 24 24" fill="currentColor">
+    <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z" />
+  </svg>
+</div>
 <div className="tabs">
       {/* This is the INACTIVE tab. Clicking it switches to 'riders' */}
       <span 
@@ -369,12 +373,21 @@ const handleImageUpload = (e) => {
       {selectedRider && (
         <div className="modal-overlay">
           <div className="modal-content">
-            <button
-              className="close-btn"
-              onClick={() => setSelectedRider(null)}
-            >
-              ✖
-            </button>
+
+            <button className="close-btn" onClick={() => setSelectedRider(null)}>
+  <svg 
+    width="24" 
+    height="24" 
+    viewBox="0 0 24 24" 
+    fill="none" 
+    stroke="currentColor" 
+    strokeLinecap="round" 
+    strokeLinejoin="round"
+  >
+    <line x1="18" y1="6" x2="6" y2="18"></line>
+    <line x1="6" y1="6" x2="18" y2="18"></line>
+  </svg>
+</button>
 
             <div className="modal-header">
               {/* === AVATAR UPLOAD SECTION === */}
@@ -442,12 +455,23 @@ const handleImageUpload = (e) => {
       {selectedOrder && !showFailedReasonModal && (
         <div className="modal-overlay">
           <div className="modal-content delivery-modal">
-            <button
-              className="close-btn"
-              onClick={() => setSelectedOrder(null)}
-            >
-              ✖
-            </button>
+           
+            <button className="close-btn" onClick={() => setSelectedOrder(null)}>
+  <svg 
+    width="24" 
+    height="24" 
+    viewBox="0 0 24 24" 
+    fill="none" 
+    stroke="currentColor"
+    strokeWidth="3" 
+    strokeLinecap="round" 
+    strokeLinejoin="round"
+  >
+    <line x1="18" y1="6" x2="6" y2="18"></line>
+    <line x1="6" y1="6" x2="18" y2="18"></line>
+  </svg>
+</button>
+
 
             <div className="modal-header">
               <h2>Delivery Details</h2>
