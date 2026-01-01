065c95e617bd633c83d2e5551e36bdd80b39ac7d
StephanieNiccoleCalawod <stephanieniccolecalawod@gmail.com>
2026-01-01 21:22:09 +0800

    removed unnecesarry datas

--- Program.cs
+++ Program.cs
@@ -284,40 +284,7 @@ app.MapPost("/api/customers/{orderId}/feedback", async (int orderId, CustomerFee
 }).WithName("SubmitCustomerFeedback");
 
 // ---------------------------
-// Minimal API Endpoint
-// ---------------------------
-
-app.MapGet("/weatherforecast", () =>
-{
-    var summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild",
-        "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast(
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
-// ---------------------------
 // Run the App
 // ---------------------------
 
 app.Run();
-
-// ---------------------------
-// Record for Forecast
-// ---------------------------
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
